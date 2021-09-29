39e6ab1467fb620e0c36a90e1a3bbeff89b882e4
Taylor Smith <tsmith@tsmithcreative.com>
2021-09-28 23:32:23 -0500

    Implement react-router-dom and basic switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import { Route, BrowserRouter, Switch, Link } from 'react-router-dom';
+
 import {
   AppBar,
   Box,
@@ -14,12 +18,8 @@ import {
 } from '@mui/material';
 
 import MenuIcon from '@mui/icons-material/Menu';
-import HomeIcon from '@mui/icons-material/Home';
 
-import React from 'react';
-
-// @TODO: Temporary. Swap this out with a router.
-import Home from './screens/Home';
+import { Routes } from './routes';
 
 const drawerWidth = 240;
 
@@ -35,66 +35,81 @@ function App() {
       <Toolbar />
       <Divider />
       <List>
-        <ListItem button key={1}>
-          <ListItemIcon><HomeIcon /></ListItemIcon>
-          <ListItemText>Home</ListItemText>
-        </ListItem>
+        {Routes.map((route: any, index: number) => {
+          // @TODO: Well this is super gross. Apparently MUI and react-router-dom don't get along
+          return (
+            <Link to={route.path}>
+              <ListItem button key={index}>
+                <ListItemIcon><route.icon /></ListItemIcon>
+                <ListItemText>{route.title}</ListItemText>
+              </ListItem>
+            </Link>
+          )
+        })}
       </List>
     </React.Fragment>
   );
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" component="div" sx={{
-        width: { sm: `calc(100% - ${drawerWidth}px)` },
-        ml: { sm: `${drawerWidth}px` },
-      }}>
-        <Toolbar>
-          <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}>
-                <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>RNF Location Admin</Typography>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav" sx={{
-        width: { sm: drawerWidth },
-        flexShrink: { sm: 0 },
-      }}>
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{ keepMounted: true }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}>
+    <BrowserRouter>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" component="div" sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}>
+          <Toolbar>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: 'none' } }}>
+                  <MenuIcon />
+            </IconButton>
+            <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>RNF Location Admin</Typography>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav" sx={{
+          width: { sm: drawerWidth },
+          flexShrink: { sm: 0 },
+        }}>
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{ keepMounted: true }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}>
 
-          {navDrawer}
+            {navDrawer}
 
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {navDrawer}
-        </Drawer>
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            open
+          >
+            {navDrawer}
+          </Drawer>
+        </Box>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Switch>
+            {Routes.map((route: any) => (
+              <Route exact path={route.path} key={route.path}>
+                <route.screen />
+              </Route>
+            ))}
+          </Switch>
+        </Box>
       </Box>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-        <Home />
-      </Box>
-    </Box>
+    </BrowserRouter>
   );
 }
 
