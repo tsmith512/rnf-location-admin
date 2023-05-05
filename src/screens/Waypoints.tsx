20412e52d43816b0ffc4a87d39edb45e027a4f7e
Taylor Smith <tsmith@tsmithcreative.com>
2023-05-04 20:40:01 -0500

    Rebuilt Waypoints as an FC

--- src/screens/Waypoints.tsx
+++ src/screens/Waypoints.tsx
@@ -17,73 +17,43 @@ import {
 } from '@mui/material';
 
 import EditLocationIcon from '@mui/icons-material/EditLocation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class Waypoints extends React.Component<{}, {
-  isError: boolean,
-  isLoaded: boolean,
-  start: number,
-  page: number,
-  rowsPerPage: number,
-  detailsOpen: boolean,
-  detailsWaypoint: any, /* @TODO: Define a Waypoint type */
-  waypoints: Array<any>,
-}> {
-  map!: google.maps.Map;
+export default function Waypoints() {
+// class Waypoints extends React.Component<{}, {
+  const [isError, setError] = useState(false as boolean);
+  const [isLoaded, setLoaded] = useState(false as boolean);
+  const [start, setStart] = useState(0 as number);
+  const [page, setPage] = useState(0 as number);
+  const [rowsPerPage, setRowsPerPage] = useState(50 as number);
+  const [detailsOpen, setDetailsOpen] = useState(false as boolean);
+  /* @TODO: Define a Waypoint type */
+  const [detailsWaypoint, setDetailsWaypoint] = useState(null as any);
+  const [waypoints, setWaypoints] = useState([] as any[]);
 
+  useEffect(() => {
+    getWaypoints();
+  }, [page, rowsPerPage]);
 
-  constructor(props: any) {
-    super(props);
+  const handleChangePage = (e: React.MouseEvent<HTMLButtonElement> | null, input: number) => {
+    setPage(input);
+  };
 
-    this.state = {
-      isError: false,
-      isLoaded: false,
-      start: 0,
-      page: 0,
-      rowsPerPage: 50,
-      detailsOpen: false,
-      detailsWaypoint: null,
-      waypoints: [],
-    };
+  const handleChageRowsPerPage = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRowsPerPage(parseInt(e.target.value));
+  };
 
-    this.handleChageRowsPerPage = this.handleChageRowsPerPage.bind(this);
-    this.handleChangePage = this.handleChangePage.bind(this);
-    this.handleDetailsOpen = this.handleDetailsOpen.bind(this);
-    this.handleDetailsClose = this.handleDetailsClose.bind(this);
-    this.getWaypoints = this.getWaypoints.bind(this);
+  const handleDetailsOpen = (waypoint: any) => {
+    setDetailsOpen(true);
+    setDetailsWaypoint(waypoint);
+  };
+
+  const handleDetailsClose = () => {
+    setDetailsOpen(false);
+    setDetailsWaypoint(null);
   }
 
-  componentDidMount() {
-    this.getWaypoints();
-  }
-
-  handleChangePage(e: React.MouseEvent<HTMLButtonElement> | null, input: number) {
-    this.setState({
-      page: input,
-    }, this.getWaypoints);
-  }
-
-  handleChageRowsPerPage(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-    this.setState({
-      rowsPerPage: parseInt(e.target.value),
-    }, this.getWaypoints);
-  }
-
-  handleDetailsOpen(waypoint: any) {
-    this.setState({
-      detailsOpen: true,
-      detailsWaypoint: waypoint,
-    });
-  }
-
-  handleDetailsClose() {
-    this.setState({
-      detailsOpen: false,
-      detailsWaypoint: null,
-    });
-  }
-
-  modalStyle = {
+  const modalStyle = {
     position: 'absolute' as 'absolute',
     top: '50%',
     left: '50%',
@@ -92,9 +62,7 @@ class Waypoints extends React.Component<{}, {
     boxShadow: 24,
   }
 
-  async getWaypoints() {
-    const { page, rowsPerPage } = this.state;
-
+  const getWaypoints = async () => {
     const start = page * rowsPerPage;
     const end = start + rowsPerPage;
 
@@ -107,87 +75,73 @@ class Waypoints extends React.Component<{}, {
     .then(res => res.json())
     .then(
       (payload) => {
-        this.setState({
-          waypoints: payload,
-          isLoaded: true,
-        });
+        setWaypoints(payload);
+        setLoaded(true);
       },
       (error) => {
         console.log(error);
-        return [];
+        setError(true);
+        setLoaded(false);
       }
     );
   }
 
-  render() {
-    const {
-      isLoaded,
-      page,
-      rowsPerPage,
-      waypoints,
-      detailsOpen,
-      detailsWaypoint,
-    } = this.state;
+  return (
+    <Container maxWidth="md">
+      {isLoaded && <Typography variant="h2" component="h2" gutterBottom>Waypoints</Typography> }
+      {!isLoaded && <Typography variant="h2" component="h2" gutterBottom>Loading</Typography> }
 
-    return (
-      <Container maxWidth="md">
-        {isLoaded && <Typography variant="h2" component="h2" gutterBottom>Waypoints</Typography> }
-        {!isLoaded && <Typography variant="h2" component="h2" gutterBottom>Loading</Typography> }
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Timestamp</TableCell>
+              <TableCell>Location</TableCell>
+              <TableCell>Label</TableCell>
+              <TableCell>State</TableCell>
+              <TableCell>Details</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {waypoints.map((waypoint: any) => {
+              const date = new Date(waypoint.timestamp * 1000)
+              return (
+                <TableRow key={waypoint.timestamp} hover={true}>
+                  <TableCell>{date.toLocaleTimeString()}<br />{date.toDateString()}</TableCell>
+                  <TableCell>{waypoint.lon}, {waypoint.lat}</TableCell>
+                  <TableCell>{waypoint.label}</TableCell>
+                  <TableCell>{waypoint.state}<br />{waypoint.country}</TableCell>
+                  <TableCell><IconButton aria-label="details" onClick={() => {handleDetailsOpen(waypoint)}}><EditLocationIcon></EditLocationIcon></IconButton></TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+        <TablePagination
+          component="div"
+          /* @TODO: Can we get a quick count of all? This is dumb. */
+          count={10000000}
+          page={page}
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChageRowsPerPage} />
+      </TableContainer>
 
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Timestamp</TableCell>
-                <TableCell>Location</TableCell>
-                <TableCell>Label</TableCell>
-                <TableCell>State</TableCell>
-                <TableCell>Details</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {waypoints.map((waypoint: any) => {
-                const date = new Date(waypoint.timestamp * 1000)
-                return (
-                  <TableRow key={waypoint.timestamp} hover={true}>
-                    <TableCell>{date.toLocaleTimeString()}<br />{date.toDateString()}</TableCell>
-                    <TableCell>{waypoint.lon}, {waypoint.lat}</TableCell>
-                    <TableCell>{waypoint.label}</TableCell>
-                    <TableCell>{waypoint.state}<br />{waypoint.country}</TableCell>
-                    <TableCell><IconButton aria-label="details" onClick={() => {this.handleDetailsOpen(waypoint)}}><EditLocationIcon></EditLocationIcon></IconButton></TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
-          <TablePagination
-            component="div"
-            /* @TODO: Can we get a quick count of all? This is dumb. */
-            count={10000000}
-            page={page}
-            onPageChange={this.handleChangePage}
-            rowsPerPage={rowsPerPage}
-            onRowsPerPageChange={this.handleChageRowsPerPage} />
-        </TableContainer>
-
-        <Modal
-          open={detailsOpen}
-          onClose={this.handleDetailsClose}
-        >
-          <Box sx={this.modalStyle}>
-            <Card>
-              <CardContent>
-                <Typography variant="h6" component="h2">Details</Typography>
-                <pre>
-                  {JSON.stringify(detailsWaypoint, null, 2)}
-                </pre>
-              </CardContent>
-            </Card>
-          </Box>
-        </Modal>
-      </Container>
-    )
-  }
+      <Modal
+        open={detailsOpen}
+        onClose={handleDetailsClose}
+      >
+        <Box sx={modalStyle}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6" component="h2">Details</Typography>
+              <pre>
+                {JSON.stringify(detailsWaypoint, null, 2)}
+              </pre>
+            </CardContent>
+          </Card>
+        </Box>
+      </Modal>
+    </Container>
+  );
 }
-
-export default Waypoints;
